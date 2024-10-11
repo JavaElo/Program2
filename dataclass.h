654c328f4e94da693929da31d00890ec8292022a
JavaElo <ccaskey43@tntech.edu>
2024-10-11 15:10:16 -0500

    dataclass

---
+++ dataclass.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class List{
+    public: 
+
+}
---
+++ driver.cpp
@@ -0,0 +1,10 @@
+#include "linkedlist.h"
+#include "smartpointer.h"
+#include "dataclass.h"
+
+int main(){
+
+
+
+    return 0;
+}
