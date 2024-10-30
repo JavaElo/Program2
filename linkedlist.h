8d55bd3112e916eb12726b6176eee22938c35d9c
JavaElo <ccaskey43@tntech.edu>
2024-10-29 23:42:12 -0500

    append function and destructor. SIDE NOTE the text file will only be used as a reference. We will not need code to access the file

--- linkedlist.h
+++ linkedlist.h
@@ -1,36 +1,73 @@
-#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
+#ifndef linkedlist_H
+#define linkedlist_H
 
-template<typename T>
-class LinkedList{
-    public:
-    LinkedList(){
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+template <typename T>
+class LinkedList
+{
+private:
+    struct ListNode
+    {
+        T value;
+        ListNode* next;
+    };
+
+    ListNode* head;
+    ListNode* tail;
+
+public:
+    LinkedList()
+    {
         head = NULL;
         tail = NULL;
     }
     ~LinkedList();
-    void appendNode();
-    void deleteNode();
+    void appendNode(T value);
+    void deleteNode(int position);
+};
+
+
+template <typename T>
+void LinkedList<T>::appendNode(T value) {
+    ListNode* node = new ListNode;
+    node->value = value;
+    node->next = NULL;
+    if (head == NULL) {
+        head = node;
+        tail = node;
+    }
+    else {
+        tail->next = node;
+        tail = node;
+    }
+}
+
+
+template <typename T>
+LinkedList<T>::~LinkedList() {
+    ListNode* nodeC = head;
+    while (nodeC != NULL) {
+        ListNode* nodeT = nodeC;
+        cout << "*****DELETING the node with address: " << nodeT << endl << endl;
+        nodeC = nodeC->next;
+        delete nodeT;
+    }
+}
+
+
+
+/*
     void GetFront();
     void GetBack();
     void GetFrom();
     void insertFront();
     void insertBack();
     //Friend sort fucntion
-
-
-    private:
-    LinkedList* head;
-    LinkedList* tail;
-
-    struct ListNode{
-        T value;
-        ListNode* next;
-    }
-};
-
-
-
-
+    //Sort function for descending
+*/
 
 #endif
