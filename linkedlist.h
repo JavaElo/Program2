e88bb29892ab037a154421a1b5cd308e7a5ca004
David Wallace <dwwallace43@tntech.edu>
2024-11-01 00:12:51 -0500

    worked on 2 sorting algorithms. need to talk with you about what we need to do

--- linkedlist.h
+++ linkedlist.h
@@ -17,6 +17,10 @@ class LinkedList{
     private:
     LinkedList* head;
     LinkedList* tail;
+    friend void quickSortMid();
+    friend void selectionSort();
+    friend void MergeSort(); 
+
 
     struct ListNode{
         T value;
@@ -24,8 +28,42 @@ class LinkedList{
     }
 };
 
+void quickSortMid( int low, int high)
+{
+    int pivotLocation = 0;
+    if( low >= high)
+    {
+        return;
+    }
 
+    pivotLocation = partition(low, high);
+    quickSortMid(low, pivotLocation);
+    quickSortMid( pivotLocation + 1, high);
+}
 
+int partition(int left, int right)
+{
+    int pivot, middle, temp;
+    bool done = false;
 
+    middle = left + (right-left) / 2;
+    int l = left;
+    int r = right;
+
+}
+
+void selectionSort()
+{
+    int minIndex, minValue;
+    int temp;
+
+    for(int start = 0; start < (size -1 ); start++)
+    {
+        minIndex = start;
+        minValue = x;
+
+        for(int i = start + 1; i < size; i++)
+    }
+}
 
 #endif
